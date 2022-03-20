6ae503942fb14234229c8c0316b2842086a40cb0
Szentgyörgyi Péter <peti0202@windowslive.com>
2022-03-20 19:57:29 +0100

    menü és elérhetőségek szekció átalakítva

---
+++ assets/javascript/app.js
@@ -0,0 +1,7 @@
+document.getElementById('nav-btn').addEventListener('click', function () {
+    document.getElementById('navi').style.height = '100vh';
+});
+
+document.getElementById('close').addEventListener('click', function () {
+    document.getElementById('navi').style.height = '0vh';
+});
