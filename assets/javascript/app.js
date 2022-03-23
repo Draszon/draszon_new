dfd85bfdfa9d09b3e8bbe802a5252c80417f84a7
Szentgyörgyi Péter <peti0202@windowslive.com>
2022-03-23 22:07:34 +0100

    menü és köv section

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,11 @@
 document.getElementById('nav-btn').addEventListener('click', function () {
-    document.getElementById('navi').style.height = '100vh';
+    document.getElementById('navi').style.height = '15.375rem';
+    document.getElementById('nav-btn').style.visibility = 'hidden';
+    document.getElementById('close').style.visibility = 'visible';
 });
 
 document.getElementById('close').addEventListener('click', function () {
-    document.getElementById('navi').style.height = '0vh';
+    document.getElementById('navi').style.height = '0rem';
+    document.getElementById('nav-btn').style.visibility = 'visible';
+    document.getElementById('close').style.visibility = 'hidden';
 });
